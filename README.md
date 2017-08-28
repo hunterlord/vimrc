c42d37af5d723393c5de5a45d7fcbfd7d86aac06
gaohd <gaohd@noahsi.com>
2017-08-28 12:03:14 +0900

    ddd

---
+++ README.md
@@ -0,0 +1,6 @@
+### install
+
+```
+git clone https://github.com/hunterlord/vimrc.git ~/.vim_runtime_hunter
+sh ~/.vim_runtime_hunter/install_vimrc.sh
+```
--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 set -e
 
+cat ~/.vim_runtime_hunter/vimrc ~/.vimrc
+
+echo "Install vundle"
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim -c 'PluginInstall' -c 'qa!'
+
+echo "vundle install success && plugins install success"
+
+mkdir ~/.vim/colors
+
+cp ~/.vim/bundle/gruvbox/colors/gruvbox.vim ~/.vim/colors/
+
+echo "vim theme gruvbox install success"
+
+
+
+echo "Vimrc install success"
