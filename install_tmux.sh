0988fea90510c029995a18a69c8982fae8caad7e
gaohd <gaohd@noahsi.com>
2017-09-12 15:55:11 +0900

    fix

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cp ~/.vim_runtime_hunter/tmux_config ~/.tmux.conf
+
+echo "tmux config set success"
--- install_zsh.sh
+++ install_zsh.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i "s/ZSH_THEME=\"[^\"]*\"/ZSH_THEME=\"af-magic\"/p" ~/.zshrc
-source ~/.zshrc
+/bin/bash -c "source ~/.zshrc"
+
+echo ""
+
