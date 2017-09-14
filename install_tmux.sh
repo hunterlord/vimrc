0e048294397c128f1a268de4509d0613d187191a
gaohd <gaohd@noahsi.com>
2017-09-14 10:48:54 +0900

    tmux session resurrect

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cp ~/.vim_runtime_hunter/tmux_config ~/.tmux.conf
 
-echo "tmux config set success"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+
+
+echo "tmux config set success, Press prefix + I (capital I, as in Install) to fetch the plugin."
